9aeaab3529fc10ed3ef056505c852f6a873edb3b
daniello13 <daniel.artemyev@outlook.com>
2019-05-10 14:13:05 +0300

    синхронизация с сервером

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 import pygame
 from pygame.locals import *
-
+import urllib.request
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
-verticies = (
-    (0.3, -0.3, -0.3),
-    (0.3, 0.3, -0.3),
-    (-0.3, 0.3, -0.3),
-    (-0.3, -0.3, -0.3),
-    (0.3, -0.3, 0.3),
-    (0.3, 0.3, 0.3),
-    (-0.3, -0.3, 0.3),
-    (-0.3, 0.3, 0.3),
-    )
-
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
-
-def Cube_creation(x,y,z,length):
+def Cube_creation(x,y,z,length, level):
     #x,y,z=-0.3 , 
+    if level==1 :
+        glColor3f(0.0,1.0,0.0)
+    if level==2:
+        glColor3f(1.0,1.0,0.0)
+    if level==3:
+        glColor3f(1.0,0.0,0.0)
     glBegin(GL_POLYGON)
     glVertex3f( x, y, z)    # P1
     glVertex3f( x,  y+length, z)     # P2
@@ -75,518 +55,58 @@ def Cube_creation(x,y,z,length):
     glVertex3f( x, y, z )
     glEnd()
     
+mas=[1,2,3,2,2,2,1,1,1,3,3,3,1,1,1,2,3,1,2,3,1,2,2,2,2,3,1]
 def Cube():
-    '''
-    glBegin(GL_POLYGON|GL_QUADS)
-    for edge in edges:
-        for vertex in edge:
-            glColor3d(1,0,0)
-            glVertex3fv(verticies[vertex])
-    glEnd()
-    '''
-    '''
-    # кубик №1
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.3, -0.3)    # P1
-    glVertex3f( -0.3,  0.3, -0.3)     # P2
-    glVertex3f(  0.3,  0.3, -0.3)      # P3
-    glVertex3f(  0.3, -0.3, -0.3)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  0.3, -0.3, 0.3 )
-    glVertex3f(  0.3,  0.3, 0.3 )
-    glVertex3f( -0.3,  0.3, 0.3 )
-    glVertex3f( -0.3, -0.3, 0.3 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, -0.3, -0.3 )
-    glVertex3f( 0.3,  0.3, -0.3 )
-    glVertex3f( 0.3,  0.3,  0.3 )
-    glVertex3f( 0.3, -0.3,  0.3 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.3,  0.3 )
-    glVertex3f( -0.3,  0.3,  0.3 )
-    glVertex3f( -0.3,  0.3, -0.3 )
-    glVertex3f( -0.3, -0.3, -0.3 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f(  0.3,  0.3,  0.3 )
-    glVertex3f(  0.3,  0.3, -0.3 )
-    glVertex3f( -0.3,  0.3, -0.3 )
-    glVertex3f( -0.3,  0.3,  0.3 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f(  0.3, -0.3, -0.3 )
-    glVertex3f(  0.3, -0.3,  0.3 )
-    glVertex3f( -0.3, -0.3,  0.3 )
-    glVertex3f( -0.3, -0.3, -0.3 )
-    glEnd()
-    
-    #2 кубик
-    #
-    #glVertex3f( 0.3, -0.3, -0.3 )
-    #glVertex3f( 0.3,  0.3, -0.3 )
-    #glVertex3f( 0.3,  0.3,  0.3 )
-    #glVertex3f( 0.3, -0.3,  0.3 )
-    glBegin(GL_POLYGON)
-    glColor3f(   1.0,  0.0,  0.0 )
-    glVertex3f( -0.3, -0.3, -0.5)    # P1
-    glVertex3f( -0.3,  0.3, -0.5)     # P2
-    glVertex3f(  0.3,  0.3, -0.5)      # P3
-    glVertex3f(  0.3, -0.3, -0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.3, -1.1)    # P1
-    glVertex3f( -0.3,  0.3, -1.1)     # P2
-    glVertex3f(  0.3,  0.3, -1.1)      # P3
-    glVertex3f(  0.3, -0.3, -1.1)       # P4
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, -0.3, -0.5)    # P
-    glVertex3f(  0.3,  0.3, -0.5)      # P3
-    glVertex3f( 0.3,  0.3, -1.1) 
-    glVertex3f(  0.3, -0.3, -1.1)       # P4
-    glEnd()
-    #
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.3, -0.5)    # P
-    glVertex3f(  -0.3,  0.3, -0.5)      # P3
-    glVertex3f( -0.3,  0.3, -1.1) 
-    glVertex3f(  -0.3, -0.3, -1.1)       # P4
-    glEnd()
-    #
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, 0.3, -0.5)    # P
-    glVertex3f(  0.3,  0.3, -0.5)      # P3
-    glVertex3f( 0.3,  0.3, -1.1) 
-    glVertex3f( -0.3, 0.3, -1.1)       # P4
-    glEnd()
-    #
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.3, -0.5)    # P
-    glVertex3f(  0.3,  -0.3, -0.5)      # P3
-    glVertex3f( 0.3,  -0.3, -1.1) 
-    glVertex3f( -0.3, -0.3, -1.1)       # P4
-    glEnd()
-    # 3 кубик
-    glBegin(GL_POLYGON)
-    glColor3f(   1.0,  0.0,  0.0 )
-    glVertex3f( -0.3, -0.3, 0.5)    # P1
-    glVertex3f( -0.3,  0.3, 0.5)     # P2
-    glVertex3f(  0.3,  0.3, 0.5)      # P3
-    glVertex3f(  0.3, -0.3, 0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.3, 1.1)    # P1
-    glVertex3f( -0.3,  0.3, 1.1)     # P2
-    glVertex3f(  0.3,  0.3, 1.1)      # P3
-    glVertex3f(  0.3, -0.3, 1.1)       # P4
-    glEnd()
-
-    #
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, -0.3, 0.5)    # P
-    glVertex3f(  0.3,  0.3, 0.5)      # P3
-    glVertex3f( 0.3,  0.3, 1.1) 
-    glVertex3f(  0.3, -0.3, 1.1)       # P4
-    glEnd()
-    #
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.3, 0.5)    # P
-    glVertex3f(  -0.3,  0.3, 0.5)      # P3
-    glVertex3f( -0.3,  0.3, 1.1) 
-    glVertex3f(  -0.3, -0.3, 1.1)       # P4
-    glEnd()
-    #
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, 0.3, 0.5)    # P
-    glVertex3f(  0.3,  0.3, 0.5)      # P3
-    glVertex3f( 0.3,  0.3, 1.1) 
-    glVertex3f( -0.3, 0.3, 1.1)       # P4
-    glEnd()
-    #
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.3, 0.5)    # P
-    glVertex3f(  0.3,  -0.3, 0.5)      # P3
-    glVertex3f( 0.3,  -0.3, 1.1) 
-    glVertex3f( -0.3, -0.3, 1.1)       # P4
-    glEnd()
-
-    #4 кубик
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.5, -0.3, -0.3)    # P1
-    glVertex3f( 0.5,  0.3, -0.3)     # P2
-    glVertex3f(  1.1,  0.3, -0.3)      # P3
-    glVertex3f(  1.1, -0.3, -0.3)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  0.5, -0.3, 0.3 )
-    glVertex3f(  0.5,  0.3, 0.3 )
-    glVertex3f( 1.1,  0.3, 0.3 )
-    glVertex3f( 1.1, -0.3, 0.3 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.5, 0.3, -0.3 )
-    glVertex3f( 0.5,  -0.3, -0.3 )
-    glVertex3f( 0.5,  -0.3,  0.3 )
-    glVertex3f( 0.5, 0.3,  0.3 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( 1.1, -0.3,  0.3 )
-    glVertex3f( 1.1,  0.3,  0.3 )
-    glVertex3f( 1.1,  0.3, -0.3 )
-    glVertex3f( 1.1, -0.3, -0.3 )
-    glEnd()
-    
-    
-    #5 кубик
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.5, -0.3, -0.5)    # P1
-    glVertex3f( 0.5,  0.3, -0.5)     # P2
-    glVertex3f(  1.1,  0.3, -0.5)      # P3
-    glVertex3f(  1.1, -0.3, -0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  0.5, -0.3, -1.1 )
-    glVertex3f(  0.5,  0.3, -1.1 )
-    glVertex3f( 1.1,  0.3, -1.1 )
-    glVertex3f( 1.1, -0.3, -1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.5, 0.3, -0.5 )
-    glVertex3f( 0.5,  -0.3, -0.5 )
-    glVertex3f( 0.5,  -0.3,  -1.1 )
-    glVertex3f( 0.5, 0.3,  -1.1 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( 1.1, -0.3,  -1.1 )
-    glVertex3f( 1.1,  0.3,  -1.1 )
-    glVertex3f( 1.1,  0.3, -0.5 )
-    glVertex3f( 1.1, -0.3, -0.5 )
-    glEnd()
-    # 6 кубик 
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.5, -0.3, 0.5)    # P1
-    glVertex3f( 0.5,  0.3, 0.5)     # P2
-    glVertex3f(  1.1,  0.3, 0.5)      # P3
-    glVertex3f(  1.1, -0.3, 0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  0.5, -0.3, 1.1 )
-    glVertex3f(  0.5,  0.3, 1.1 )
-    glVertex3f( 1.1,  0.3, 1.1 )
-    glVertex3f( 1.1, -0.3, 1.1)
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.5, 0.3, 0.5 )
-    glVertex3f( 0.5,  -0.3, 0.5 )
-    glVertex3f( 0.5,  -0.3,  1.1 )
-    glVertex3f( 0.5, 0.3,  1.1 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( 1.1, -0.3,  1.1 )
-    glVertex3f( 1.1,  0.3,  1.1 )
-    glVertex3f( 1.1,  0.3, 0.5 )
-    glVertex3f( 1.1, -0.3, 0.5 )
-    glEnd()
-    # 7 кубик
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.5, -0.3, -0.3)    # P1
-    glVertex3f( -0.5,  0.3, -0.3)     # P2
-    glVertex3f(  -1.1,  0.3, -0.3)      # P3
-    glVertex3f(  -1.1, -0.3, -0.3)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.5, -0.3, 0.3 )
-    glVertex3f(  -0.5,  0.3, 0.3 )
-    glVertex3f( -1.1,  0.3, 0.3 )
-    glVertex3f( -1.1, -0.3, 0.3 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.5, 0.3, -0.3 )
-    glVertex3f( -0.5,  -0.3, -0.3 )
-    glVertex3f( -0.5,  -0.3,  0.3 )
-    glVertex3f( -0.5, 0.3,  0.3 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( -1.1, -0.3,  0.3 )
-    glVertex3f( -1.1,  0.3,  0.3 )
-    glVertex3f( -1.1,  0.3, -0.3 )
-    glVertex3f( -1.1, -0.3, -0.3 )
-    glEnd()
-
-    # 8 кубик
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.5, -0.3, -0.5)    # P1
-    glVertex3f( -0.5,  0.3, -0.5)     # P2
-    glVertex3f(  -1.1,  0.3, -0.5)      # P3
-    glVertex3f(  -1.1, -0.3, -0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.5, -0.3, -1.1 )
-    glVertex3f(  -0.5,  0.3, -1.1 )
-    glVertex3f( -1.1,  0.3, -1.1 )
-    glVertex3f( -1.1, -0.3, -1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.5, 0.3, -0.5 )
-    glVertex3f( -0.5,  -0.3, -0.5 )
-    glVertex3f( -0.5,  -0.3,  -1.1 )
-    glVertex3f( -0.5, 0.3,  -1.1 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( -1.1, -0.3,  -1.1 )
-    glVertex3f( -1.1,  0.3,  -1.1 )
-    glVertex3f( -1.1,  0.3, -0.5 )
-    glVertex3f( -1.1, -0.3, -0.5 )
-    glEnd()
-    # 9 кубик
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.5, -0.3, 0.5)    # P1
-    glVertex3f( -0.5,  0.3, 0.5)     # P2
-    glVertex3f(  -1.1,  0.3, 0.5)      # P3
-    glVertex3f(  -1.1, -0.3, 0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.5, -0.3, 1.1 )
-    glVertex3f(  -0.5,  0.3, 1.1 )
-    glVertex3f( -1.1,  0.3, 1.1 )
-    glVertex3f( -1.1, -0.3, 1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.5, 0.3, 0.5 )
-    glVertex3f( -0.5,  -0.3, 0.5 )
-    glVertex3f( -0.5,  -0.3,  1.1 )
-    glVertex3f( -0.5, 0.3,  1.1 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( -1.1, -0.3,  1.1 )
-    glVertex3f( -1.1,  0.3,  1.1 )
-    glVertex3f( -1.1,  0.3, 0.5 )
-    glVertex3f( -1.1, -0.3, 0.5 )
-    glEnd()
-
-    #10 КУБИК
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, 0.5, -0.3)    # P1
-    glVertex3f( 0.3,  0.5, -0.3)     # P2
-    glVertex3f(  0.3,  1.1, -0.3)      # P3
-    glVertex3f(  -0.3, 1.1, -0.3)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.3, 0.5, 0.3 )
-    glVertex3f(  0.3,  0.5, 0.3 )
-    glVertex3f( 0.3, 1.1, 0.3 )
-    glVertex3f( -0.3, 1.1, 0.3 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.5, -0.3 )
-    glVertex3f( -0.3,  -1.1, -0.3 )
-    glVertex3f( -0.3,  -1.1,  0.3 )
-    glVertex3f( -0.3, -0.5,  0.3 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, -0.5,  -0.3 )
-    glVertex3f( 0.3,  -1.1,  -0.3 )
-    glVertex3f( 0.3,  -1.1, 0.3 )
-    glVertex3f( 0.3, -0.5, 0.3 )
-    glEnd()
-
-    #11 cube
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, 0.5, -0.5)    # P1
-    glVertex3f( 0.3,  0.5, -0.5)     # P2
-    glVertex3f(  0.3,  1.1, -0.5)      # P3
-    glVertex3f(  -0.3, 1.1, -0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.3, 0.5, -1.1 )
-    glVertex3f(  0.3,  0.5, -1.1 )
-    glVertex3f( 0.3, 1.1, -1.1 )
-    glVertex3f( -0.3, 1.1, -1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.5, -0.5 )
-    glVertex3f( -0.3,  -1.1, -0.5 )
-    glVertex3f( -0.3,  -1.1,  -1.1 )
-    glVertex3f( -0.3, -0.5,  -1.1 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, -0.5,  -0.5 )
-    glVertex3f( 0.3,  -1.1,  -0.5 )
-    glVertex3f( 0.3,  -1.1, -1.1 )
-    glVertex3f( 0.3, -0.5, -1.1 )
-    glEnd()
-    #12 cube
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, 0.5, 0.5)    # P1
-    glVertex3f( 0.3,  0.5, 0.5)     # P2
-    glVertex3f(  0.3,  1.1, 0.5)      # P3
-    glVertex3f(  -0.3, 1.1, 0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.3, 0.5, 1.1 )
-    glVertex3f(  0.3,  0.5, 1.1 )
-    glVertex3f( 0.3, 1.1, 1.1 )
-    glVertex3f( -0.3, 1.1, 1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.5, 0.5 )
-    glVertex3f( -0.3,  -1.1, 0.5 )
-    glVertex3f( -0.3,  -1.1,  1.1 )
-    glVertex3f( -0.3, -0.5,  1.1 )
-    glEnd()
-    
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, -0.5,  0.5 )
-    glVertex3f( 0.3,  -1.1, 0.5 )
-    glVertex3f( 0.3,  -1.1, 1.1 )
-    glVertex3f( 0.3, -0.5, 1.1 )
-    glEnd()
-    #13 cube -0.3
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.5, -0.3)    # P1
-    glVertex3f( 0.3,  -0.5, -0.3)     # P2
-    glVertex3f(  0.3,  -1.1, -0.3)      # P3
-    glVertex3f(  -0.3, -1.1, -0.3)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.3, -0.5, 0.3 )
-    glVertex3f(  0.3,  -0.5, 0.3 )
-    glVertex3f( 0.3, -1.1, 0.3 )
-    glVertex3f( -0.3, -1.1, 0.3 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, 0.5,  -0.3 )
-    glVertex3f( -0.3,  1.1,  -0.3 )
-    glVertex3f( -0.3,  1.1, 0.3 )
-    glVertex3f( -0.3, 0.5, 0.3 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, 0.5,  -0.3 )
-    glVertex3f( 0.3,  1.1,  -0.3 )
-    glVertex3f( 0.3,  1.1, 0.3 )
-    glVertex3f( 0.3, 0.5, 0.3 )
-    glEnd()
-    #14 cube
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.5, -0.5)    # P1
-    glVertex3f( 0.3,  -0.5, -0.5)     # P2
-    glVertex3f(  0.3,  -1.1, -0.5)      # P3
-    glVertex3f(  -0.3, -1.1, -0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.3, -0.5, -1.1 )
-    glVertex3f(  0.3,  -0.5, -1.1 )
-    glVertex3f( 0.3, -1.1, -1.1 )
-    glVertex3f( -0.3, -1.1, -1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, 0.5,  -0.5 )
-    glVertex3f( -0.3,  1.1,  -0.5 )
-    glVertex3f( -0.3,  1.1, -1.1 )
-    glVertex3f( -0.3, 0.5, -1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, 0.5,  -0.5 )
-    glVertex3f( 0.3,  1.1,  -0.5 )
-    glVertex3f( 0.3,  1.1, -1.1 )
-    glVertex3f( 0.3, 0.5, -1.1 )
-    glEnd()
-    #15 cube
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, -0.5, 0.5)    # P1
-    glVertex3f( 0.3,  -0.5, 0.5)     # P2
-    glVertex3f(  0.3,  -1.1,0.5)      # P3
-    glVertex3f(  -0.3, -1.1,0.5)       # P4
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f(  -0.3, -0.5, 1.1 )
-    glVertex3f(  0.3,  -0.5, 1.1 )
-    glVertex3f( 0.3, -1.1, 1.1 )
-    glVertex3f( -0.3, -1.1, 1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( -0.3, 0.5,  0.5 )
-    glVertex3f( -0.3,  1.1,  0.5 )
-    glVertex3f( -0.3,  1.1, 1.1 )
-    glVertex3f( -0.3, 0.5, 1.1 )
-    glEnd()
-
-    glBegin(GL_POLYGON)
-    glVertex3f( 0.3, 0.5,  0.5 )
-    glVertex3f( 0.3,  1.1,  0.5 )
-    glVertex3f( 0.3,  1.1, 1.1 )
-    glVertex3f( 0.3, 0.5, 1.1 )
-    glEnd()
-    '''
     edge_length=0.6
     distance_between_small_cubes=0.2
-    z = -1.1
+
     glColor3f(   1.0,  0.0,  0.0 )
-    coord = [-1.1,z+edge_length+distance_between_small_cubes,0.5]
-    for z in coord:
-        Cube_creation(-0.3,-0.3,z,edge_length) #A
-        Cube_creation( 0.5, -0.3, z , edge_length) #B
-        Cube_creation( -0.3, 0.5, z , edge_length) #C
-        Cube_creation(0.5,0.5,z, edge_length) #D
-        Cube_creation(-0.3,-1.1,z, edge_length )#E
-        Cube_creation(0.5, -1.1, z, edge_length)#F
-        Cube_creation(-1.1,0.5, z, edge_length) #G
-        Cube_creation(-1.1,-0.3,z,edge_length) #H
-        Cube_creation(-1.1, -1.1, z, edge_length) #I
+    
+    Cube_creation(-1.1,-1.1,-1.1,edge_length, mas[0]) #A
+    Cube_creation( -1.1, -0.3, -1.1 , edge_length,mas[1]) #B
+    Cube_creation( -1.1, 0.5, -1.1 , edge_length,mas[2]) #C
+    Cube_creation(-0.3,-1.1,-1.1, edge_length,mas[3]) #D
+    Cube_creation(-0.3,-0.3,-1.1, edge_length,mas[4] )#E
+    Cube_creation(-0.3, 0.5, -1.1, edge_length,mas[5])#F
+    Cube_creation(0.5,-1.1, -1.1, edge_length,mas[6]) #G
+    Cube_creation(0.5,-0.3,-1.1,edge_length,mas[7]) #H
+    Cube_creation(0.5, 0.5, -1.1, edge_length,mas[8]) #I
+
+    Cube_creation(-1.1,-1.1,-0.3,edge_length,mas[9]) #A
+    Cube_creation( -1.1, -0.3, -0.3 , edge_length,mas[10]) #B
+    Cube_creation( -1.1, 0.5, -0.3 , edge_length,mas[11]) #C
+    Cube_creation(-0.3,-1.1,-0.3, edge_length,mas[12]) #D
+    Cube_creation(-0.3,-0.3,-0.3, edge_length ,mas[13])#E
+    Cube_creation(-0.3, 0.5, -0.3, edge_length,mas[14])#F
+    Cube_creation(0.5,-1.1, -0.3, edge_length,mas[15]) #G
+    Cube_creation(0.5,-0.3,-0.3,edge_length,mas[16]) #H
+    Cube_creation(0.5, 0.5, -0.3, edge_length,mas[17]) #I
+
+    Cube_creation(-1.1,-1.1,0.5,edge_length,mas[18]) #A
+    Cube_creation( -1.1, -0.3, 0.5 , edge_length,mas[19]) #B
+    Cube_creation( -1.1, 0.5, 0.5 , edge_length,mas[20]) #C
+    Cube_creation(-0.3,-1.1,0.5, edge_length,mas[21]) #D
+    Cube_creation(-0.3,-0.3,0.5, edge_length ,mas[22])#E
+    Cube_creation(-0.3, 0.5, 0.5, edge_length,mas[23])#F
+    Cube_creation(0.5,-1.1, 0.5, edge_length,mas[24]) #G
+    Cube_creation(0.5,-0.3,0.5,edge_length,mas[25]) #H
+    Cube_creation(0.5, 0.5, 0.5, edge_length,mas[26]) #I
     
     #`Cube_creation(-0.5, -0.5, -0.5, edge_length)
     #Cube_creation()
+def get_data():
+    global mas
+    f = urllib.request.urlopen('http://diplom.moneto4ka.tk/send_data')
+    s=(str)( f.read())
+    counter = 0
+    counter2=0
+    for i in s:
+        counter2+=1
+        if i ==" ":
+            mas[counter]=(int)(s[counter2-2])
+            counter+=1
+    #print(mas)
 
-
-def keyboard (Key, x, y):
-    if(Key==GLUT_KEY_RIGHT):
-        glRotatef(2, 0, 0, 1)
-    
 
 def main():
     motion = "stop"
@@ -599,7 +119,7 @@ def main():
     #glLoadIdentity()
     gluPerspective(45, (display[0]/display[1]), 0.3, 50.0)
     #gluLookAt(10, 10, 10, 0, 0, 0, 0, 100, 0)
-    glTranslatef(0.0,0.0, -5)
+    glTranslatef(0.0,0.0, -6.0)
     
     while True:
         for event in pygame.event.get():
@@ -630,13 +150,13 @@ def main():
             glRotatef(1, 0, 0, 1)
         if motion == "up":
             glRotatef(1,1,0,0)
-        #glRotatef(1, 3, 1, 1)
+        glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-
+        get_data()
         Cube()
         
         pygame.display.flip()
-        pygame.time.wait(10)
+        pygame.time.wait(1)
 
 
 main()
